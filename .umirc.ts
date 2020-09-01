f397e58d534c181644a6e9f5c8015daf2eab77bc
murphy.meng <murphy.meng@connext.com.cn>
2020-09-01 16:01:22 +0800

    open layout

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  layout: {},
+  routes: [{ path: '/', component: '@/pages/index' }],
 });
